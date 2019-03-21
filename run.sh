994ae4372450a9237eaf5c016523b6cc05ed26fb
Riey <creeper844@gmail.com>
2019-03-22 03:57:54 +0900

    Add nodefaults option to qemu

--- run.sh
+++ run.sh
@@ -19,6 +19,7 @@ cargo xbuild $CARGO_ARGS &&
 cp $PWD/target/$TARGET/$BUILD_TYPE/qni-os.efi $PWD/esp/EFI/Boot/BootX64.efi &&
 
 qemu-system-x86_64 \
+    -nodefaults \
     -vga std -serial stdio \
     -machine q35,accel=kvm:tcg -m 128M \
     -drive if=pflash,format=raw,file="$OVMF_DIR/OVMF_CODE.fd",readonly=on \
